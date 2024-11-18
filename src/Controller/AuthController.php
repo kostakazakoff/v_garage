042097eedfb526415ebeb58de11031d2e3a21246
Kosta Kazakov <kostakazakoff@gmail.com>
2024-11-18 06:10:11 +0200

    DashboardController created, JWT test

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,12 +5,12 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/auth/', name: 'api_auth_')]
+#[Route('/api/auth', name: 'api_auth_')]
 class AuthController extends AbstractController
 {
     public function __construct(
@@ -19,8 +19,8 @@ class AuthController extends AbstractController
         )
     {
     }
-    #[Route('register', name: 'register', methods: ['POST'])]
-    public function register(Request $request): Response
+    #[Route('/register', name: 'register', methods: ['POST'])]
+    public function register(Request $request): JsonResponse
     {
         $credentials = json_decode($request->getContent());
         $email = $credentials->email;
